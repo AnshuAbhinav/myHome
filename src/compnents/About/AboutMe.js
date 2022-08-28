7a170821e49b79d4b3f49a0f909a78feaaed04ed
Abhinav <abhinavstarr@gmail.com>
2022-08-29 00:22:05 +0530

    CSS fixes for text area in contact pages

--- src/compnents/About/AboutMe.js
+++ src/compnents/About/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
                 <div className='heading'>About Myself,</div>
                 <div className='detail'>I am a Front-End developer from India. I am passionate about creating UI, animation effects and creating a intuitive and dynamic user experience</div>
                 <div className='detail'>Working as <span style={{color: 'orange', fontWeight: 'bold'}}>Application Development Senior Analyst</span> in <span style={{color: 'orange', fontWeight: 'bold'}}>Accenture</span> with some of the most brilliant minds to provide best solution for the clients with cutting edge technology.</div>
-                <div className='detail'>Well organized, problem solver, helpful, supportive and self moivated person. Love photography, travelling and listening soft music.</div>
+                <div className='detail'>Well organized, problem solver, helpful, supportive and self motivated person. Love photography, travelling and listening soft music.</div>
             </div>
         </div>
     )
